84cf29ec4185c24ad70a1c166a9cc74177aa5d3c
DangBui <buiminhdangjp@gmail.com>
2024-06-29 21:16:26 +0900

    test2

--- api/weather.js
+++ api/weather.js
@@ -3,6 +3,10 @@ import fetch from 'node-fetch';
 export default async function handler(req, res) {
     const { lat, lon } = req.query;
     const apiKey = process.env.OPENWEATHER_API_KEY;
+    if (!apiKey) {
+        res.status(500).json({ error: 'API key is missing in environment variables' });
+        return;
+    }
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
     try {
